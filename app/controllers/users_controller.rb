2270b942f95d79382365a5be1d0a70dd8fabbcd7
Aram Anderson <aram.j.anderson@gmail.com>
2017-09-10 22:36:29 -0600

    Fix create user issue

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < BaseController
-  before_action :current_user?, except: :new
+  before_action :current_user?, except: [:new, :create]
 
   def show
     @user = User.find_by(id: params[:id])
@@ -13,11 +13,11 @@ class UsersController < BaseController
   end
 
   def create
-    user = User.new(user_params)
-    if user.save
-      session[:user_id] = user.id
-      flash[:message] = "#{user.username} was successfully logged in"
-      redirect_to user_path(user)
+    @user = User.new(user_params)
+    if @user.save
+      session[:user_id] = @user.id
+      flash[:message] = "#{@user.username} was successfully logged in"
+      redirect_to user_path(@user)
     else
       flash[:message] = "Unable to create user"
       render :new
